a77c39ca0ddeaef043e58bed62388476f3e25a00
Uday Bhaskar Gajavalli <udaygajavalli@gmail.com>
2021-01-10 08:11:31 +0530

    Initital

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularfirst';
+  error = '';
+
+  count:number = 1000;
+  handleIncrease () {
+    this.count = this.count + 10;
+  }
+
+  handleDecrease = () =>{
+    if(this.count === 0){
+        console.log("Can't Decrease");
+    }else{
+      this.count = this.count - 1;
+    }
+  }
+
+  handleReset = () => {
+    this.count = 0;
+  }
 }
