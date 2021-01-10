b485c432a27a3e93a246b8a979981fd6d96bdf15
Uday Bhaskar Gajavalli <udaygajavalli@gmail.com>
2021-01-10 10:42:39 +0530

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angularfirst';
   error = '';
 
-  count:number = 1000;
-  handleIncrease () {
+  count: number = 1000;
+  handleIncrease = () => {
     this.count = this.count + 10;
-  }
+  };
 
-  handleDecrease = () =>{
-    if(this.count === 0){
-        console.log("Can't Decrease");
-    }else{
+  handleDecrease = () => {
+    if (this.count === 0) {
+      console.log("Can't Decrease");
+    } else {
       this.count = this.count - 1;
     }
-  }
+  };
 
   handleReset = () => {
     this.count = 0;
-  }
+  };
 }
